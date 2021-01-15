d857e4746f2befdb937b786b341eea37dab13a63
boriskrusteff <boris@krusteff.com>
2021-01-15 20:24:36 +0200

    v2

--- main.py
+++ main.py
@@ -5,10 +5,13 @@ from selenium.webdriver.common.keys import Keys
 
 
 class Instagram_Automator:
-    browser = webdriver.Chrome()
-    browser.get("https://instagram.com")
+    options = webdriver.ChromeOptions()
+    prefs = {"profile.default_content_setting_values.notifications": 1}
+    options.add_experimental_option("prefs", prefs)
+    browser = webdriver.Chrome(options=options)
 
     def login(self, emailz, passwordz):
+        self.browser.get("https://instagram.com")
         accept_button = self.browser.find_element_by_xpath("/html/body/div[2]/div/div/div/div[2]/button[1]")
         accept_button.click()
         email = self.browser.find_element_by_xpath(
@@ -46,11 +49,45 @@ class Instagram_Automator:
         self.browser.close()
 
 
-messages = ("тест2", "Как си Алекс", "???")
-alex = Instagram_Automator()
-alex.login("boris@krusteff.com", "falcon1312")
-alex.find_person("alex_3618")
-for i in range(len(messages)):
-    alex.send_message(messages[i])
-alex.exit_instagram()
+class Facebook_Automator:
+    options = webdriver.ChromeOptions()
+    prefs = {"profile.default_content_setting_values.notifications": 1}
+    options.add_experimental_option("prefs", prefs)
+    browser = webdriver.Chrome(options=options)
 
+    def login(self, fb_email, fb_password):
+        self.browser.get("https://facebook.com")
+        accept_all = self.browser.find_element_by_xpath('//*[@id="u_0_h"]')
+        accept_all.click()
+        email = self.browser.find_element_by_xpath('//*[@id="email"]')
+        email.send_keys(fb_email)
+        password = self.browser.find_element_by_xpath('//*[@id="pass"]')
+        password.send_keys(fb_password)
+        login_button = self.browser.find_element_by_xpath('//*[@id="u_0_b"]')
+        login_button.click()
+        time.sleep(3)
+
+    def find_peoples(self, username):
+        self.browser.get("https://www.facebook.com/search/people/?q=" + urllib.parse.quote(username))
+        time.sleep(3)
+
+    def find_posts(self, post_name):
+        self.browser.get("https://www.facebook.com/search/posts?q=" + urllib.parse.quote(post_name))
+        time.sleep(3)
+
+    def find_photos(self, photo_name):
+        self.browser.get("https://www.facebook.com/search/photos?q=" + urllib.parse.quote(photo_name))
+        time.sleep(3)
+
+    def find_videos(self, video_name):
+        self.browser.get("https://www.facebook.com/search/videos?q=" + urllib.parse.quote(video_name))
+        time.sleep(3)
+
+    def find_pages(self, page_name):
+        self.browser.get("https://www.facebook.com/search/pages?q=" + urllib.parse.quote(page_name))
+        time.sleep(3)
+
+
+facebooker = Facebook_Automator()
+facebooker.login("boris@krusteff.com", "falcon1312")
+facebooker.find_peoples("Plamen Adarsky")
